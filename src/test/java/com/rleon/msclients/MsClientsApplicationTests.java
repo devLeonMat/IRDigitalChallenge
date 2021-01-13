179edde9913761e37be59a27abf56c5844527cf1
leon matias roslin <leonmatias1991@gmail.com>
2021-01-13 16:18:32 -0500

    Version Inicial

---
+++ src/test/java/com/rleon/msclients/MsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
